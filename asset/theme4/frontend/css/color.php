8baa8cd13489a60106e655fc2d874d04c3b2a11c
stephen gideon <gideonbus14@gmail.com>
2024-05-10 15:47:47 +0100

    added login,forgot password.policy,and register

---
+++ asset/theme4/frontend/css/color.php
@@ -0,0 +1,48 @@
+
+:root {
+    --main-color: #;
+}
+
+.feature-box .content {
+    box-shadow: inset 0 0px 66px #47;
+}
+
+.invest-plan-top::before {
+    box-shadow: inset 0 0px 66px #d4;
+}
+
+.invest-plan::before {
+    box-shadow: inset 0 0px 66px #3b;
+}
+
+.invest-plan-shape::after {
+    background-color: #33;
+}
+
+.invest-plan:hover .invest-plan-shape::after {
+    background-color: #;
+}
+
+.work-box {
+    box-shadow: inset 0 0px 20px #c4;
+}
+
+.work-box .icon {
+    box-shadow: inset 0 0px 20px #c4;
+}
+
+.cmn-table.style-separate tbody tr {
+    box-shadow: inset 0 0px 8px #4d;
+}
+
+.testimonial-box .content {
+    box-shadow: inset 0 0px 20px #c4;
+}
+
+.referral-box::before {
+    box-shadow: inset 0 0px 20px #c4;
+}
+
+.referral-box-step {
+    box-shadow: inset 0 0px 6px #9e;
+}
