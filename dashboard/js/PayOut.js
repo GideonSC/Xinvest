3a74569ce66a2346af2d21114c64423cf9b0d230
stephen gideon <gideonbus14@gmail.com>
2024-05-14 15:31:30 +0200

    DONE!

--- dashboard/js/PayOut.js
+++ dashboard/js/PayOut.js
@@ -18,3 +18,30 @@ signin.addEventListener("click", () => {
     loginin.classList.add("block");
     loginup.classList.add("none");
 })
+
+document.getElementById('copyButton').addEventListener('click', function() {
+    var addressText = document.querySelector('.Address__box').innerText.trim();
+    copyToClipboard(addressText);
+    displayAlert();
+  });
+  
+  function copyToClipboard(text) {
+    var textarea = document.createElement("textarea");
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  }
+  
+  function displayAlert() {
+    var alertElement = document.getElementById('alertMessage');
+    alertElement.classList.remove('hidden');
+    setTimeout(function() {
+      alertElement.classList.add('hidden');
+    }, 4000); // Hide the alert after 3 seconds
+  }
+  
+  
+  
+  
